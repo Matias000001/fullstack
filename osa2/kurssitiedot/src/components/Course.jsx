5391906fe3f81806660e63b479c80a37b9c126a9
Matias Palmroth <matias.palmroth@helsinki.fi>
2025-11-06 16:12:57 +0200

    Exercises 2.1-2.5

---
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,12 @@
+const Course = ({ course }) => (
+  <div>
+    <h2>{course.name}</h2>
+    {course.parts.map(part => (
+      <p key={part.id}>
+        {part.name} {part.exercises}
+      </p>
+    ))}
+  </div>
+)
+
+export default Course
