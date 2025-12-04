615ec94e63659dda34ae7dff19a1df1f8057902c
Matias Palmroth <matias.palmroth@helsinki.fi>
2025-12-04 17:18:33 +0200

    index.js factoring

--- osa3/backend/index.js
+++ osa3/backend/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 const cors = require('cors')
 app.use(cors())
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 
 var morgan = require('morgan')
