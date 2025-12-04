bf6efc60c017cdfa0fb84a1e15a740d158c421fb
Matias Palmroth <matias.palmroth@helsinki.fi>
2025-12-04 19:06:02 +0200

    zzz..fixing

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.NODE_ENV === 'production' 
-  ? 'https://puhelinluettelo-backend-nnym.onrender.com/api/persons'
-  : '/api/persons'
+const baseUrl = 'https://puhelinluettelo-backend-nnym.onrender.com/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl).then(response => response.data)
