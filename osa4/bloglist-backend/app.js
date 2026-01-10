456b6e1d5cb03e90fbe4e381296f4dea0c8ed761
Matias Palmroth <matias.palmroth@helsinki.fi>
2026-01-10 23:09:38 +0200

    Exercise 4.18

--- osa4/bloglist-backend/app.js
+++ osa4/bloglist-backend/app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const connectToMongo = require('./utils/mongo')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
@@ -12,5 +14,7 @@ connectToMongo(MONGODB_URI)
 app.use(express.json())
 app.use('/api/blogs', require('./controllers/blogs'))
 app.use('/api/users', require('./controllers/users'))
+app.use('/api/login', loginRouter)
+app.use(middleware.errorHandler)
 
 module.exports = app
